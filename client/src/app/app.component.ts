07b06df4248dd8a9fd871e6e7d4490b3e7c31efb
Zmilligan0 <90408186+Zmilligan0@users.noreply.github.com>
2024-03-14 17:58:23 -0600

    Implemented client side api request

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './Models/product';
+import { Pagination } from './Models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Topshop';
+  products: Product[] = [];
+
+  constructor(private http: HttpClient){}
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: (response) => this.products = response.data, //What to do next
+      error: error => console.log(error), // What to do if error
+      complete: () => {
+        console.log('request completed');
+        console.log('extrassssss');
+      }
+    })
+  }
 }
