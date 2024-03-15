245f0aab29db7a0dcbcf381aa2c9ddaceaaba622
Zmilligan0 <90408186+Zmilligan0@users.noreply.github.com>
2024-03-15 12:01:30 -0600

    Front-end pagination and search querying implemented

---
+++ client/src/app/shop/shop.service.ts
@@ -0,0 +1,38 @@
+import { ShopParams } from './../shared/Models/shopParams';
+import { Type } from './../shared/Models/type';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable} from '@angular/core';
+import { Pagination } from '../shared/Models/pagination';
+import { Product } from '../shared/Models/product';
+import { Brand } from '../shared/Models/brand';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopService {
+  baseUrl = 'https://localhost:5001/api/';
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(shopParams: ShopParams) {
+    let params = new HttpParams();
+
+    if (shopParams.brandId > 0) params = params.append('brandId', shopParams.brandId);
+    if (shopParams.typeId) params = params.append('typeId', shopParams.typeId);
+    params = params.append('sort', shopParams.sort);
+    params = params.append('pageIndex', shopParams.pageNumber);
+    params = params.append('pageSize', shopParams.pageSize);
+    if (shopParams.search) params = params.append('search', shopParams.search);
+
+    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {params});
+  }
+
+  getBrands() {
+    return this.http.get<Brand[]>(this.baseUrl + 'products/brands');
+  }
+
+  getTypes() {
+    return this.http.get<Type[]>(this.baseUrl + 'products/types');
+  }
+}
