340ec8a615af61bec404f3a9003727e09b871002
Zmilligan0 <90408186+Zmilligan0@users.noreply.github.com>
2024-03-16 13:50:10 -0600

    error handling and middleware bug fix

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -6,40 +6,38 @@ namespace API.Middleware
 {
     public class ExceptionMiddleware
     {
-        private readonly IHostEnvironment _env;
         private readonly RequestDelegate _next;
         private readonly ILogger<ExceptionMiddleware> _logger;
-        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        private readonly IHostEnvironment _env;
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, 
+            IHostEnvironment env)
         {
+            _env = env;
             _logger = logger;
             _next = next;
-            _env = env;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
             try
             {
-                //If no exception move onto next stage
                 await _next(context);
             }
             catch (Exception ex)
             {
-                //If in dev mode then give more details for error messages
                 _logger.LogError(ex, ex.Message);
-                context.Response.ContentType = "applicaton/json";
+                context.Response.ContentType = "application/json";
                 context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
                 var response = _env.IsDevelopment()
                     ? new ApiException((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace.ToString())
                     : new ApiException((int)HttpStatusCode.InternalServerError);
 
-
                 var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
-                
-                var json = JsonSerializer.Serialize(response);
 
-                await context.Response.WriteAsync(json); 
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
             }
         }
     }
--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
-
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'test-error', component: TestErrorComponent},
+  {path: 'not-found', component: NotFoundComponent},
+  {path: 'server-error', component: ServerErrorComponent},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule)},
-  {path: '**', redirectTo: '', pathMatch: 'full'}, // invalid route, return to home
+  {path: '**', redirectTo: '', pathMatch: 'full'},
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
