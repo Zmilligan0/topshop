6f5829a99c488798b64c866602052927e7166312
Zmilligan0 <90408186+Zmilligan0@users.noreply.github.com>
2024-03-24 13:30:43 -0600

    Configuring API data validation

--- API/Program.cs
+++ API/Program.cs
@@ -17,19 +17,19 @@ builder.Services.AddControllers();
 //Implements the applications services from the extensions folder
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
+ builder.Services.AddSwaggerDocumentation();
+
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<ExceptionMiddleware>();
 
 
 app.UseStatusCodePagesWithReExecute("/errors/{0}");
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwaggerDocumentation();
 
 //Serve static files
 app.UseStaticFiles();
