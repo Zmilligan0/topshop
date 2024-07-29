ba7e92a85ce5c9c2e35544b2e31d4e66587548cf
Zmilligan0 <90408186+Zmilligan0@users.noreply.github.com>
2024-07-29 15:15:28 -0600

    Order history and summary implementation

--- client/src/app/checkout/checkout-success/checkout-success.component.ts
+++ client/src/app/checkout/checkout-success/checkout-success.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Order } from 'src/app/shared/Models/order';
 
 @Component({
   selector: 'app-checkout-success',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class CheckoutSuccessComponent {
 
+  order?: Order;
+
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    this.order = navigation?.extras?.state as Order
+  }
 }
